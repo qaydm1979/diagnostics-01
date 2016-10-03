23f7182e81fbe533f2fcf86922d190d08ffe222d
Peiwu Qin <peiwu@berkeley.edu>
2016-10-03 10:51:58 -0600

    calc_dvar.py was added.

--- scripts/calc_dvars.py
+++ scripts/calc_dvars.py
@@ -1,8 +1,8 @@
 """ Script to calculate dvars values
 
 For testing run::
-
-    python3 scripts/calc_dvars.py tests/small_4d.nii
+    cd test
+    python3 ..//scripts/calc_dvars.py small_4d.nii
 
 You should see:
 
@@ -29,7 +29,7 @@ If you make that work, check that the tests pass by:
 """
 
 import sys
-
+import os
 import numpy as np
 
 import nibabel as nib
@@ -37,7 +37,6 @@ import nibabel as nib
 
 def calc_image_dvars(img):
     """ Root mean squared difference between volumes in `img`.
-
     Parameters
     ----------
     img : image object
@@ -56,13 +55,26 @@ def calc_image_dvars(img):
     # Square the differences;
     # Sum over voxels for each volume, and divide by the number of voxels;
     # Return the square root of these values.
-    raise RuntimeError('No code yet')
-
+    data=img.get_data()
+    print("The dimension of data is ", data.shape)
+    t=data.shape[-1]
+    vol0=data[...,0].ravel()
+    N=len(vol0)
+    shape=[]
+    for i in range(t-1):
+        distance=data[...,i] - data[...,i+1]
+        d_sqrt=np.sum(distance**2)
+        shape.append(np.sqrt(d_sqrt/N))
+    # raise RuntimeError('No code yet')
+    print('length of shape vecor is ', len(shape))
+    return shape
 
 def main():
     # Get the first command line argument
+
     filename = sys.argv[1]
-    img = nib.load(filename)
+
+    img = nib.load(filename, mmap=False)
     print(calc_image_dvars(img))
 
 
