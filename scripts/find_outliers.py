0d876ae0f90be01d73f109f398748090a839e009
Peiwu Qin <peiwu@berkeley.edu>
2016-10-10 12:22:08 -0600

    Minor updates.

--- scripts/find_outliers.py
+++ scripts/find_outliers.py
@@ -1,100 +1,103 @@
 """ Python script to find outliers
-
 Run as:
-
     python3 scripts/find_outliers.py data
 """
 
+# get imports
 import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
-def find_outliers(data_directory):
+
+# if modules folder, set to sys.path
+if os.path.isdir('packages'):
+    if os.path.abspath('packages') not in sys.path:
+        # add to sys.path
+        sys.path.append(os.path.abspath('packages'))
+# import detectors
+from detectors import *
+from grid_2d import *
+
+project_path = os.getcwd()
+dirs = [project_path + '\\result', \
+        project_path + '\\result/fig',\
+        project_path + '\\result\\fig\\mosaic']
+
+for d in dirs:
+    if not os.path.exists(d):
+        os.mkdir(d)
+
+def find_outliers(data_path):
     """ Print filenames and outlier indices for images in `data_directory`.
-
     Print filenames and detected outlier indices to the terminal.
-
     Parameters
     ----------
-    data_directory : str
-        Directory containing containing images.
-
+    data_path : str
+        Directory containing containing images or path to specific file.
     Returns
     -------
-    None
+    outliers : data dictionary
+        Dictionary with each file and its corresponding outliers
     """
-    path_cwd = os.getcwd()
-    path = os.path.join(path_cwd,data_directory)
-    os.chdir(path)
-    img = nib.load('group01_sub01_run1.nii', mmap=False)
-    data = img.get_data()
-    t = data.shape[-1]   #time points
-    N = len(data[...,0].ravel()) #The number of voxels per volume
 
-    print("Please select th)e method for outlier detection.")
-    print("1: statistical (parametric) test")
-    print("2: distance (non-parametric) based approach")
-    print("3: Density (Cluster) based approach")
-    print("4: High demiension adapted approach")
-    print("5: outlier by standard variation")
-    method = input(">")
-    print('Your selected method is ', method)
-    # print(type(method))
-    if int(method) == 1:
-        Mohalanobis_distance = []
-        for i in range(t):
-            # Z=data.shape[2]
-            # for j in range(Z):
-            #     print('Peiwu')
-            X=data[:,:,:,i].ravel()
-            unscaled_cov = X.dot(X.T) #This is covariance matrix for each volume
-            # Mohalanobis distance for each multivariate data points i=1...., n is
-            # denoted by Mi and given by:
-            mean_4d = np.mean(data)
-            X_mean_sub = X - mean_4d
-            X_mean_unscaled_cov = X_mean_sub.T.dot(unscaled_cov)
-            X_mean_cov = X_mean_unscaled_cov.dot(X_mean_sub)
-            Mohalanobis_distance.append(np.sqrt(X_mean_cov))
-        Mohalanobis_array = np.array(Mohalanobis_distance)
-        # print("The shape of Mohalanobis_array is", Mohalanobis_array.shape)
-        # The volume that has higher Mohalanobis index can be regarded as outliers
-        max_i = np.argmax(Mohalanobis_array)
-        outliers_array = Mohalanobis_array < Mohalanobis_array[max_i]
-        plt.figure(1)
-        plt.plot(Mohalanobis_array)
-        plt.show()
-        # print(outliers_array)
-        data_remove_outliers = data[...,outliers_array]
-        return data_remove_outliers
-
-    elif int(method) == 2:
-        print(method)
-    elif int(method) == 3:
-        print(method)
-    elif int(method) == 4:
-        print(method)
-    elif int(method) == 5:
-        vol_std = []
-        for i in range(t):
-            vol_std.append(data[...,i].std())
-        plt.figure(2)
-        plt.plot(np.array(vol_std))
-        plt.show()
-        #The std plot is very similar to Mohalanobis methos.
-        min_i = np.argmin(np.array(vol_std))
-        max_i = np.argmax(np.array(vol_std))
-        outliers_array = (vol_std > vol_std[min_i])
-        outliers_array[max_i] = False
-        print(outliers_array)
-        data_remove_outliers = data[...,outliers_array]
-        return data_remove_outliers
-
+    # init data_dict
+    data_dict = {}
+    # ensure data_path is fullpath
+    data_path = os.path.abspath(data_path)
+    # check if data_path is directory or file
+    if os.path.isdir(data_path):
+        # return nii files
+        data_files = []
+        for f in os.listdir(data_path):
+            if f[-4:] == '.nii':
+                data_files.append(os.path.join(data_path,f))
+    elif os.path.isfile(data_path):
+        # set data_files to [data_path]
+        data_files = [data_path]
     else:
-        RuntimeError("please make a resonable choice")
-    # Your code here
-    # raise RuntimeError('No code yet')
+        # error
+        raise('Directory does not exist or unknown file')
 
+    # for each fileName, load image and find outliers
+    for fileName in data_files:
+        # get img and data
+        img = nib.load(fileName, mmap=False)
+        data = img.get_data()
+
+        # Make the mosaic plot for the data
+
+        name = fileName[-22:]
+        plt.imshow(plot_mosaic(data[...,83]), cmap = 'gray', alpha = 1)
+        plt.colorbar()
+        plt.title(name)
+        plt.savefig(project_path + '\\result\\fig\\mosaic\\' + name + '.png')
+        plt.close()
+
+        # init outliers
+        outliers = []
+        # check for mean outliers
+        vol_mean, tmp = mean_detector(data)
+        outliers.extend(tmp)
+        # check for std outliers
+        vol_std, tmp = std_detector(data)
+        outliers.extend(tmp)
+        # check for Mahalanobis outliers
+        #
+        # D, tmp = mah_detector(data)
+        # outliers.extend(tmp)
+        # check for rms dvars outliers
+        dvars, tmp = dvars_detector(data)
+        outliers.extend(tmp)
+        # PCA?
+
+        # get unique outliers
+        outliers = list(set(outliers))
+        # set data_dict
+        data_dict[fileName] = sorted(outliers)
+
+    # return the data dictionary with outliers
+    return data_dict
 
 def main():
     # This function (main) called when this file run as a script.
@@ -105,8 +108,11 @@ def main():
                            "command line")
     data_directory = sys.argv[1]
     # Call function to validate data in data directory
-    find_outliers(data_directory)
+    data_dict = find_outliers(data_directory)
 
+    # print data dictionary of fileNames and outliers
+    for key in data_dict:
+        print(os.path.basename(key), data_dict[key])
 
 if __name__ == '__main__':
     # Python is running this file as a script, not importing it.
